dcf6debeff599031995841df7989b8a1ea37db7b
amiksayed <amiksayed34@gmail.com>
2022-10-12 14:16:53 +0600

    fixed netlify reload issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,14 @@ import { Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase_init";
 import { signOut } from "firebase/auth";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  let activeStyle = {
+    color: "#00a9b1",
+    fontWeight: "bold"
+  };
+
   const [user]= useAuthState(auth);
   const handleSignOut=()=>{
     signOut(auth)
@@ -18,11 +24,19 @@ const Header = () => {
         <img src={logo} alt="" />
       </div>
       <div className="nav-link">
-        <Link to="/home">Home</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/about">About</Link>
+        <NavLink to="/home" style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            } > Home </NavLink>
+        <NavLink to="/services" style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>Services</NavLink>
+        <NavLink to="/about" style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>About</NavLink>
         {
-          user?<Link onClick={handleSignOut} to="/login">Log out</Link>: <Link to="/login">Sign in</Link>
+          user?<Link onClick={handleSignOut} to="/login">Log out</Link>: <NavLink to="/login" style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>Sign in</NavLink>
         }
         <Link className="book" to="/booking">BOOK NOW</Link>
       </div>
